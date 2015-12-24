59d2a77c82c5fc54222e7734e38c7ae4e3dab05f
Jonathan Rodriguez <son.of.yhwh@gmail.com>
2015-12-24 11:16:14 -0500

    With socket.io

--- gameserver.js
+++ gameserver.js
@@ -1,28 +1,22 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var compression = require('compression');
 
 app.set('port', (process.env.PORT || 1080));
 
+server.listen(app.get('port'));
+
+app.use(compression());
+
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-/*app.get('/', function(req, res) {
-  //if(req.url == "/game") {
-    var actualFileName = "game.html";
-    //console.log(req.url+", "+actualFileName+", "+"200"+", "+counter)
-    fs.readFile(actualFileName, function(err, text){
-      res.setHeader("Content-Type", "text/html");
-      res.end(text);
-    });
-    //}
-  //response.render('game.html');
-});*/
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+io.on("connection", function(socket) {
+  socket.emit("handshake", {blah: "blah"});
+  socket.on("handshake2", function(data) {
+    console.log("handshake2: " + data);
+  });
 });
 
-
+console.log("Applandia! server started on port " + app.get('port'));
